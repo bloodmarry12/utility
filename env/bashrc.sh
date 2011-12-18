4e1107edeb48d3b266fe14461d68bb5b42223377
gavin geng <gavingeng@hotmail.com>
2011-12-18 12:31:26 +0800

    增加逻辑处理生成main函数处理的判断逻辑，只有0为非0的情况下不生成main函数

--- env/bashrc.sh
+++ env/bashrc.sh
@@ -9,28 +9,38 @@ mkcd(){
 }
 
 touchsh(){
-    if test ! -e $1
-    then
-        touch $1
-        chmod 755 $1
-        #title="#!/bin/bash -x \n#autuor `whoami` \n#date:`date +"%Y-%m-%d %T"` \n"
-        title="#!/bin/bash -x \n#filename:$1\n#autuor:\tgavingeng\n#date:\t\t`date +"%Y-%m-%d %T"`\n"
-        echo -e $title >>$1
-    fi  
+	if test ! -e $1
+	then
+    	touch $1
+		chmod 755 $1
+    	#title="#!/bin/bash -x \n#autuor `whoami` \n#date:`date +"%Y-%m-%d %T"` \n"
+    	title="#!/bin/bash -x \n#filename:$1\n#autuor:\tgavingeng\n#date:\t\t`date +"%Y-%m-%d %T"`\n"
+    	echo -e $title >>$1
+	fi
 
-    vi $1
+	vi $1
 }
 
 touchpy(){
-    if test ! -e $1
-    then
-        touch $1
-        chmod 755 $1
-        title="#!/usr/bin/env python\n#coding:utf-8\n#filename:$1\n\n'''\nauthor:\tgavingeng\ndate:\t`date +"%Y-%m-%d %T"` \n'''"
-        title=${title}"\n\n\ndef main():\n\tpass\n\nif __name__=='__main__':\n\tmain()"
-        #title="#coding:utf-8\n\n#author:\tgavingeng\n#date:\t\t`date +"%Y-%m-%d %T"` \n"
-        echo -e $title >>$1
-    fi  
+	if test ! -e $1
+	then
+		touch $1
+		chmod 755 $1
+		title="#!/usr/bin/env python\n#coding:utf-8\n#filename:$1\n\n'''\nauthor:\tgavingeng\ndate:\t`date +"%Y-%m-%d %T"` \n'''"
 
-    vi $1
+		generate_main=0 #default  0
+
+		if test $# = 2;
+		then
+			generate_main=$2
+		fi
+
+		if test $generate_main = 0 ;then
+			title=${title}"\n\n\ndef main():\n\tpass\n\nif __name__=='__main__':\n\tmain()"
+		fi
+
+		echo -e $title >>$1
+	fi
+
+	vi $1
 }
